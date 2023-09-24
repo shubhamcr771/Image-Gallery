bca58b554d9b8e8723d9e7f3d337adbc2a253be7
Shubham <shubhamsingh705010@gmail.com>
2023-09-24 17:10:57 +0530

    image-darken

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+// Declare an array listing the filenames of each image
+const imageFiles = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
-/* Declaring the array of image filenames */
+// Declare an object listing the alternative text for each image
+const imageName = {
+  "pic1.jpg": "Image 1",
+  "pic2.jpg": "Image 2",
+  "pic3.jpg": "Image 3",
+  "pic4.jpg": "Image 4",
+  "pic5.jpg": "Image 5",
+};
 
-/* Declaring the alternative text for each image file */
+// Function to toggle the darken effect on the displayed image
+function toggleDarkenEffect() {
+  overlay.classList.toggle("darken-overlay");
+}
 
-/* Looping through images */
+btn.addEventListener("click", toggleDarkenEffect);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+// Loop through the array of filenames and insert <img> elements into thumb-bar
+for (const image of imageFiles) {
+  const newImage = document.createElement("img");
+  const imagePath = `./images/${image}`; // Replace 'path_to_your_images' with the actual path to your images
+  newImage.setAttribute("src", imagePath);
+  newImage.setAttribute("alt", imageName[image]);
+  thumbBar.appendChild(newImage);
 
-/* Wiring up the Darken/Lighten button */
+  // Add click event listeners to each <img> inside thumb-bar to display the corresponding image
+  newImage.addEventListener("click", function () {
+    displayedImage.setAttribute("src", imagePath);
+    displayedImage.setAttribute("alt", imageName[image]);
+  });
+}
